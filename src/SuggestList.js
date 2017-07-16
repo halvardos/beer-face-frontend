36f14439cc8306fda455d2fe1754955a16f8dcd6
halvard <hal.egg@gmail.com>
2017-07-16 19:45:05 +0200

    List all suggestions that have been made

--- src/SuggestList.js
+++ src/SuggestList.js
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SuggestList = () => (
-  <div>
-    <h1>List of suggestions</h1>
-  </div>
-)
+export default class SuggestList extends Component {
 
-export default SuggestList;
+  constructor(props) {
+    super(props);
+    this.state = {
+      suggestions: []
+    };
+  }
+
+  componentDidMount() {
+    this.getSuggestions();
+  }
+
+  async getSuggestions() {
+    const response = await fetch(`https://face-beer.herokuapp.com/api/suggestion`);
+    const suggestions = await response.json();
+
+    this.setState({
+      ...this.state,
+      suggestions,
+    });
+  }
+
+  renderSuggestionList() {
+    return this.state.suggestions.map(element => (
+      <li key={element.suggestion}>
+        <img src={element.image_url} alt="face"/>
+        <h3>{JSON.parse(element.suggestions)[0].beer.name}</h3>
+        <h4>Faces</h4>
+        <pre><code>
+          {JSON.stringify(JSON.parse(element.suggestions)[0].parsedFace)}
+        </code></pre>
+      </li>
+    ));
+  }
+
+  render() {
+    return (
+      <div>
+        <div>List of all our suggestions</div>
+        <ul>{this.renderSuggestionList()}</ul>
+      </div>
+    );
+  }
+}
